acc469058c1936359a65cbbab298e290c34f1965
edte <zzzzip6@gmail.com>
2022-10-11 09:16:01 +0800

    feat(compress): modify compress implements

--- compress/compress.go
+++ compress/compress.go
@@ -13,3 +13,19 @@ type Compressor interface {
 	Pack(data []byte) (res []byte, err error)
 	UnPack(data []byte) (res []byte, err error)
 }
+
+var Compressors = map[CompressType]Compressor{
+	None:    &RawCompressor{},
+	Gzip:    &GzipCompressor{},
+	Zlib:    &ZlipCompressor{},
+	Huffman: &HuffmanCompressor{},
+}
+
+// 自定义注册编码方案
+func RegisterCompressor(t CompressType, c Compressor) {
+	Compressors[t] = c
+}
+
+func UnRegisterCodec(t CompressType, c Compressor) {
+	delete(Compressors, t)
+}
